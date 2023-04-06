4e9806dc7b0a2551ded3a5e55580776779808493
arshappleid <arsh.appleid@gmail.com>
2023-04-06 16:21:26 -0700

    added bootstrap

--- index.php
+++ index.php
@@ -3,6 +3,11 @@
 
 <head>
 	<title>Login Page</title>
+	<!--Add bootstrap-->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 	<style>
 		body {
 			display: flex;
@@ -78,10 +83,10 @@
 
 
 	?>
-	<div class="container">
+	<div class="container text-center">
 		<h2>Login:</h2>
 
-		<form method="post" action="index.php">
+		<form class="row d-flex justify-content-center" method="post" action="index.php">
 			<div>
 				<label for="username"><b>Username</b></label>
 				<input type="text" placeholder="Enter Username" name="username" required>
